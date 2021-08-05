522ecf060bc4b199fd66ff6ed2699c6b69812fb5
Gabriel Guerra <gfranklings@gmail.com>
2021-08-05 14:53:42 -0400

    fixes

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sL https://raw.githubusercontent.com/AlphaTechnolog/archos.js/dev/create
+curl -sL https://raw.githubusercontent.com/AlphaTechnolog/archos.js/dev/create -o create
 node create $1
 rm create
