99ab87d219b3d5d0da598a92416a5f1ebb7d86fe
Gabriel Guerra <gfranklings@gmail.com>
2021-08-06 00:44:19 -0400

    removed HelloController calling (unnecesary and stupid calling)

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,10 +5,9 @@ const controller = require('../../archos/lib/controller');
 /**
  * Register your api routes in this file, the routes
  * will be prefixed by the /api path prefix.
- * 
+ *
  * @example If your path is '/' the path is /api/
  * @example If your path is /hello/world/ the path is /api/hello/world/
  */
 
 ApiRouter.get('/', () => controller.call("Api"));
-ApiRouter.get('/hello', () => controller.call("HelloController"));
